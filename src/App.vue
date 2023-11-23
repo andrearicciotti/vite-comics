f827a99e7c60f1d10b339a42a88220416a58624c
andrearicciotti <ricciotti.andrea1993@gmail.com>
2023-11-23 21:11:22 +0100

    add: header component, images in assets, update README.md

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script>
+import HeaderApp from './components/HeaderApp.vue'
+export default {
+
+  components: {
+    HeaderApp,
+  },
+
+  data() {
+    return {
+
+    }
+  },
+  methods: {},
+}
 
 </script>
 
 <template>
-
+  <HeaderApp />
 </template>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,62 @@
+<script>
+export default {
+
+    components: {},
+
+    data() {
+        return {
+            navOptions: [
+                {
+                    option: 'Characters'
+                },
+                {
+                    option: 'Comics'
+                },
+                {
+                    option: 'Movies'
+                },
+                {
+                    option: 'Tv'
+                },
+                {
+                    option: 'Games'
+                },
+                {
+                    option: 'Collectibles'
+                },
+                {
+                    option: 'Videos'
+                },
+                {
+                    option: 'Fans'
+                },
+                {
+                    option: 'News'
+                },
+                {
+                    option: 'Shop'
+                }
+            ]
+        }
+    },
+    methods: {},
+}
+
+</script>
+
+<template>
+<div class="navbar">
+    <div class="logo">
+        <img src="../assets/img/dc-logo.png" alt="">
+    </div>
+    <div class="options">
+        <ul>
+            <li v-for="(option, index) in navOptions" key="option.option">{{ option }}</li>
+        </ul>
+    </div>
+</div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
