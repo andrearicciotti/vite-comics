a96fee4f8553bc356d83959d36b11e9cf33da3a8
andrearicciotti <ricciotti.andrea1993@gmail.com>
2023-11-27 01:36:59 +0100

    add: card component

--- README.md
+++ README.md
@@ -9,3 +9,12 @@ Quando la struttura a macroblocchi è pronta, popolate le voci di menu dinamicam
 Per oggi diamo priorità alla struttura: quando è tutto bello solido, passiamo al Sass!
 
 - Creare un componente aggiuntivo per gestire la fascia azzurra con le icone.
+
+- Continuate a lavorare nella stessa repo di ieri e create un nuovo componente per visualizzare le card dei fumetti, prendendo i dati dal file json che trovate in allegato.
+Una volta inseriti tutti i contenuti dinamicamente, completate il vostro layout e rifinite i dettagli con Sass.
+
+---
+
+**Dovete utilizzare props!**
+
+---
---
+++ src/components/CardSeries.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+    props: {
+        image: String,
+        price: String,
+        series: String,
+        type: String,
+    },
+
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="col">
+        <img :src="image" :alt="series">
+        <span class="title">{{ series }}</span>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../style/partials/variables' as *;
+@use '../style/partials/mixins' as *;
+
+.col {
+    width: calc(100% / 6) - 2%;
+    height: 35%;
+
+    img {
+        height: 90%;
+        display: block;
+        aspect-ratio: 1;
+        object-fit: cover;
+        object-position: top;
+        margin-bottom: .6rem;
+    }
+
+    .title {
+        display: block;
+        color: $white_color;
+        text-transform: uppercase;
+        font-size: .6rem;
+        margin: .7rem 0;
+    }
+}
+</style>
